fac219e09534358ed8cb798c9602a01735d3e026
zireael69420 <josephcuartel.15@gmail.com>
2026-04-10 02:11:01 +0800

    limit

--- mental_wellness/settings.py
+++ mental_wellness/settings.py
@@ -63,7 +63,7 @@ WSGI_APPLICATION = 'mental_wellness.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-        conn_max_age=600,
+        conn_max_age=0,
     )
 }
 
